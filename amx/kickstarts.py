03f243e1bd002a97b5bfe0b487909f15cd26d04a
Ryan Bradley <bradleyrp@gmail.com>
2017-09-28 21:12:36 -0400

    kickstarter for testsets

--- amx/kickstarts.py
+++ amx/kickstarts.py
@@ -4,7 +4,8 @@
 KICKSTART SCRIPTS
 """
 
-kickstarters = {'all':"""
+kickstarters = {
+'all':"""
 make set module source="http://github.com/biophyscode/amx-proteins.git" spot="inputs/proteins"
 make set module source="$up/amx-extras.git" spot="inputs/extras"
 make set module source="$up/amx-docs.git" spot="inputs/docs"
@@ -16,6 +17,18 @@ make set module source="$up/amx-martini.git" spot="inputs/martini"
 make set module source="$up/amx-charmm.git" spot="inputs/charmm"
 make set module source="$up/amx-structures.git" spot="inputs/structure-repo"
 make set module source="$up/amx-polymers.git" spot="inputs/polymers"
+""",
+'testset':"""
+make set module source="http://github.com/biophyscode/amx-proteins.git" spot="inputs/proteins"
+make set module source="$up/amx-extras.git" spot="inputs/extras"
+make set module source="$up/amx-docs.git" spot="inputs/docs"
+make set commands inputs/docs/docs.py
+make set module source="$up/amx-vmd.git" spot="inputs/vmd"
+make set commands inputs/vmd/quickview.py
+make set module source="$up/amx-bilayers.git" spot="inputs/bilayers"
+make set module source="$up/amx-martini.git" spot="inputs/martini"
+make set module source="$up/amx-charmm.git" spot="inputs/charmm"
+make set module source="$up/amx-structures.git" spot="inputs/structure-repo"
 """,
 'proteins':"""
 make set module source="$up/amx-proteins.git" spot="amx/proteins"
