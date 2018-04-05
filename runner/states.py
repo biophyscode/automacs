ad5edd94f7565bfc6d1bcab1038ea267dd8c7064
Ryan Bradley <bradleyrp@gmail.com>
2018-04-04 20:46:01 -0400

    updated log list

--- runner/states.py
+++ runner/states.py
@@ -93,7 +93,8 @@ def call_reporter(func,state={}):
 		if 'step_log_file' in state:
 			#---! note that `make quick vmd_protein` needs to use the state without q in many functions
 			no_log_needed = ['init','make_sidestep','gmx_get_last_frame',
-				'gmx_get_trajectory','gmx_run','call_reporter','gmx_get_last_call','write_continue_script']
+				'gmx_get_trajectory','gmx_run','call_reporter','gmx_get_last_call','write_continue_script',
+				'get_last_mdps']
 			if 'q' not in state and func.__name__ not in no_log_needed: 
 				raise Exception('dev error: %s called the reporter '%func.__name__+
 					'before the "q" function was registered. if this is okay, add the function '+
